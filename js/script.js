0968ccc9ef3b177a639847d045a8ca5fd6e36467
ANDREY <aaazzznokia@gmail.com>
2020-07-31 15:22:26 +0400

    add steps

--- js/script.js
+++ js/script.js
@@ -24,26 +24,68 @@ for (let i = 0; i < mobMenuItems.length; i++) {
 }
 
 const counter = function () {
-    const btns = document.querySelectorAll('.counter__btn');
-  
-  
-    btns.forEach(btn => {
-      btn.addEventListener('click', function () {
-        const direction = this.dataset.direction;
-        const inp = this.parentElement.querySelector('.counter__value');
-        const currentValue = +inp.value;
-        let newValue;
-  
-        if (direction === 'plus') {
-          newValue = currentValue + 1;
-        } else {
-          newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
-        }
-  
-        inp.value = newValue;
-      })
+  const btns = document.querySelectorAll('.counter__btn');
+
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', function () {
+      const direction = this.dataset.direction;
+      const inp = this.parentElement.querySelector('.counter__value');
+      const currentValue = +inp.value;
+      let newValue;
+
+      if (direction === 'plus') {
+        newValue = currentValue + 1;
+      } else {
+        newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+      }
+
+      inp.value = newValue;
     })
-  
-  }
-  
-  counter();
+  })
+
+}
+
+counter();
+
+
+
+
+const inpForms = document.querySelectorAll('input[data-rule]')
+const stepForms = document.querySelectorAll('.step__items')
+for (let input of inpForms) {
+  input.addEventListener( 'keyup', function () {
+    let rule = this.dataset.rule
+    let value = this.value
+
+
+    if (inpForms[0].value != '') {
+      inpForms[1].classList.remove("disabled");
+      inpForms[1].removeAttribute("disabled");
+      stepForms[1].classList.add("choose-step");
+    }
+    else {
+      inpForms[1].value = ''
+      inpForms[1].classList.add("disabled");
+      inpForms[1].setAttribute("disabled", "disabled");
+      stepForms[1].classList.remove("choose-step");
+      stepForms[0].classList.add("choose-step");
+
+
+    }
+    if (inpForms[1].value != '') {
+      inpForms[2].classList.remove("disabled");
+      inpForms[2].removeAttribute("disabled");
+      stepForms[2].classList.add("choose-step");
+    }
+    else {
+      inpForms[2].value = ''
+      inpForms[2].classList.add("disabled");
+      inpForms[2].setAttribute("disabled", "disabled");
+      stepForms[2].classList.remove("choose-step");
+    }
+
+
+
+  })
+}
